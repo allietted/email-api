b2bc85580f25b8c2eed6c841d0377e89a1edb7c2
Alliette Dortelus <allietted@gmail.com>
2023-07-31 22:04:25 -0400

    added getContact

---
+++ functions/src/email.js
@@ -0,0 +1,34 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+export async function getContact(req,res){
+    const{name, email, message} = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:process.env.EMAIL_USER,
+            pass:process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    let mailOptions ={
+        from:process.env.EMAIL_USER,
+        to:process.env.EMAIL_USER,
+        subject:`Message from ${name}-${email}`,
+        text: `${message}\n\nFrom: ${email}`,
+    };
+
+    try {
+        await transponder.sendMail(mailOptions);
+        console.log('Email sent successfully');
+        res.status(200).send();
+    } catch(error){
+        console.log('Error occurs', error);
+        res.status(500).send();
+    }
+}
+
